19d7b8a8035e8857abf8b92420f7a9e48cf67069
Gino804 <giacomo.p03@gmail.com>
2023-07-05 15:42:11 +0200

    Added flags

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -5,6 +5,11 @@ export default {
         originalTitle: String,
         language: String,
         rating: Number
+    },
+    data() {
+        return {
+            imageUrl: `../src/assets/img/${this.language}.png`
+        }
     }
 }
 </script>
@@ -13,7 +18,8 @@ export default {
     <ul>
         <li>Titolo: {{ title }}</li>
         <li>Titolo originale: {{ originalTitle }}</li>
-        <li>Lingua: {{ language }}</li>
-        <li>Valutazioni: {{ rating }}</li>
+        <li>Lingua: <img :src="`../src/assets/img/${language}.png`" :alt="language"></li>
+        <li>Valutazioni:
+            {{ rating }}</li>
     </ul>
 </template>
